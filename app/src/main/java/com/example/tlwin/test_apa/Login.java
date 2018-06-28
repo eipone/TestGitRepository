7e7e81b73c527429b097e368e566dda6df78df4a
Thinzar <tzl@gmail.com>
2018-06-28 10:15:58 +0630

    my_first_commit

---
+++ app/src/main/java/com/example/tlwin/test_apa/Login.java
@@ -0,0 +1,13 @@
+package com.example.tlwin.test_apa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//test
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
